54cc283789a9944d29175dbe6ed2d79fb131e7b1
havisheth9-arch <havisheth9@gmail.com>
2026-04-12 21:26:17 -0400

    Add files via upload

---
+++ not_operator.py
@@ -0,0 +1,20 @@
+a = 10
+b = 12
+c = 12
+print(a != b)
+print(b != c)
+
+d = "python"
+e = "coding"
+if d != e :
+    print(d, 'and', e, 'are different.')
+
+f = 4
+g = 5
+if (f == 1) != (g == 5):
+    print('Hello')
+
+h = int(input("enter a number"))
+if a%2 != 0 :
+    print(h, "is not even number.")
+
---
+++ typecasting.py
@@ -0,0 +1,26 @@
+a = 9
+b = 3.6
+c = "Hello"
+d = True
+
+print(type(a) , type(False))
+print(type(b) , type("World"))
+print(type(c) , type(4.5))
+print(type(d) , type(3))
+
+print(int(b))
+print(int(c))
+print(int(d))
+print(int(2.5))
+
+print(float(a))
+print(float(c))
+print(float(d))
+
+print(str(a))
+print(str(b))
+print(str(d))
+
+print(bool(a))
+print(bool(b))
+print(bool(c))
